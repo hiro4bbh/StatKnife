ee3edcdb9f26a2d3581e8c7c6eda05a6d7061092
Tatsuhiro Aoshima <hiro4bbh@gmail.com>
2019-02-13 19:18:17 +0900

    Prepared the framework

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-"use babel";
-
+const fs = require('fs');
+const path = require('path');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-let mainWindow;
 
-app.on('window-all-closed', () => app.quit());
-
+electron.protocol.registerStandardSchemes(['es6']);
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  //mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', () => { mainWindow = null });
+  // Set MIME type 'text/javascript' for script[@type="module"].
+  electron.protocol.registerBufferProtocol('es6', ( req, cb ) => {
+    fs.readFile(
+      path.join(__dirname, req.url.replace('es6://', '')),
+      (_, b) => { cb({mimeType: 'text/javascript', data: b})}
+    )
+  })
+  // Open the main window.
+  let mainWindow = new BrowserWindow({width: 1200, height: 600, webPreferences: {nodeIntegration: true}});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.webContents.openDevTools({mode: 'left'});
 });
+app.on('window-all-closed', () => app.quit());
